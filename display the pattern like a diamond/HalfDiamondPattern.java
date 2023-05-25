4be21513e5ab6e297b9850dec93c1122f651dacf
Omkar Kanase <omkarkanase@outlook.com>
2023-05-25 10:21:51 +0530

    initial commit

--- display the pattern like a diamond/HalfDiamondPattern.java
+++ display the pattern like a diamond/HalfDiamondPattern.java
@@ -5,18 +5,16 @@ public class HalfDiamondPattern {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of rows (half of the diamond): ");
         int rows = scanner.nextInt();
-        
-        // Upper half of the diamond
+
         for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        
-        // Lower half of the diamond (excluding the middle row)
+
         for (int i = rows - 1; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
